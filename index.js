b08dd8d596dc9dd865c85f7376ac17ae21523237
reaperwatch <ofirzadka1@gmail.com>
2024-11-16 12:21:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Made by @slashing__moon", "🎮 Playing VALORANT"];
+const statusMessages = ["Made by @slashing__moon", "Made by @slashing__moon"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
